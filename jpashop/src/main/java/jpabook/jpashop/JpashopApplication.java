ef9ede93d7e0183fd2a7e6aceaf3696451b0b1c3
yujindonut <bbb3631@naver.com>
2023-02-17 12:43:23 +0900

    [CHORE] DB 설정 수정

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -8,10 +8,6 @@ public class JpashopApplication {
 
 	public static void main(String[] args) {
 
-		Hello hello = new Hello();
-		hello.setData("HII");
-		System.out.println(hello.getData());
-
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 }
---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+  @Id @GeneratedValue
+  @Column(name = "member_id")
+  private Long id;
+
+  private String name;
+  @Embedded // 내장 타입이다! 라는 것을 알아볼 수 있음
+  private Address address;
+  @OneToMany(mappedBy = "member") // Order에 있는 member에 패핑된 거울일뿐
+  private List<Order> orders = new ArrayList<>();
+}
