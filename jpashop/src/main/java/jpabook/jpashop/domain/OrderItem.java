ad6e4e741e31176b64d002b54ef8c9be8b9459b1
yujindonut <bbb3631@naver.com>
2023-02-21 02:03:38 +0900

    [FEAT] 주문, 주문상품 엔티티 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -24,7 +24,17 @@ public class OrderItem {
   private Item item; //주문 상품
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "order_id")
+
   private Order order; //주문
-  private int orderPrice; //주문 가격 private int count; //주문 수량
+  private int orderPrice; //주문 가격
+  private int count; //주문 수량
+  /* 비즈니스 로직 */
+  public void cancel() {
+    getItem().addStock(count);
+  }
+
+  public int getTotalPrice() {
+    return getOrderPrice() * getCount();
+  }
 }
 
